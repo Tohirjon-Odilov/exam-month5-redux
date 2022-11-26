7e5803c3ef56e3fb130051c22e26f1c5d3c723c3
Tohirjon-Odilov <shahanshoh819@gmail.com>
2022-11-26 20:49:41 +0500

    update files

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,49 +1,49 @@
-import React, { useState } from 'react'
-import { Link } from "react-router-dom"
-import './Header.scss'
-import MyInput from "../UI/MyInput"
-import MyList from "../UI/MyList"
-import Wrapper from "../UI/Wrapper"
-import logo from './../../assets/img/circle b.svg'
-import close from '../../assets/img/close-menu.svg'
-import menu from '../../assets/img/menu.svg'
-import { useDispatch } from "react-redux"
-import { cate } from "../../redux/generalDataSlice"
-import Modal from "../UI/MyModal/Modal"
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./Header.scss";
+import MyInput from "../UI/MyInput";
+import MyList from "../UI/MyList";
+import Wrapper from "../UI/Wrapper";
+import logo from "./../../assets/img/circle b.svg";
+import close from "../../assets/img/close-menu.svg";
+import menu from "../../assets/img/menu.svg";
+import { useDispatch } from "react-redux";
+import { cate } from "../../redux/generalDataSlice";
+import Modal from "../UI/MyModal/Modal";
 
 function Header() {
-  const [toggle, setToggle] = useState(true)
-  const dispatch = useDispatch()
-  const [modal, setModal] = useState(false)
+  const [toggle, setToggle] = useState(true);
+  const dispatch = useDispatch();
+  const [modal, setModal] = useState(false);
   const handleCategory = (data) => {
-    dispatch(cate(data))
-  }
+    dispatch(cate(data));
+  };
   const handleClick = () => {
-    setToggle(prev => !prev)
-  }
+    setToggle((prev) => !prev);
+  };
 
   return (
     <section className="site-header">
       <div className="container">
         <header className="header">
           <div className="link__wrapper">
-            <Link onClick={() => handleCategory('tenetur')} to={"/tenetur"} >
+            <Link onClick={() => handleCategory("tenetur")} to={"/tenetur"}>
               <img src={logo} alt="Bobur logo" width={58} height={58} />
               <strong>BoburBlog</strong>
             </Link>
           </div>
-          <Wrapper className={'nav__wrapper'}>
+          <Wrapper className={"nav__wrapper"}>
             <MyList />
             <MyInput isOn={setModal} />
             <Modal isOn={modal} />
           </Wrapper>
-          {<MyList className={toggle ? 'none' : 'block'} />}
+          {<MyList className={toggle ? "none" : "block"} />}
           {<img onClick={handleClick} src={toggle ? menu : close} alt="menu" />}
         </header>
         <MyInput isOn={setModal} className={"mobile-input"} />
       </div>
     </section>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,104 +1,98 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from "react-redux"
-import { allData } from "../../redux/generalDataSlice"
-import axios from "../../api/axios"
-import './MainPage.scss'
-import Aside from "../Aside/Aside"
-import Footer from "../Footer/Footer"
-import Header from "../Header/Header"
-import * as dayjs from 'dayjs'
-import { Link, useNavigate, useParams } from "react-router-dom"
-import Loading from "../UI/Loading/Loading"
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { allData } from "../../redux/generalDataSlice";
+import axios from "../../api/axios";
+import "./MainPage.scss";
+import Aside from "../Aside/Aside";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import * as dayjs from "dayjs";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import Loading from "../UI/Loading/Loading";
 
 function MainPage() {
-  const { allDatas, categories } = useSelector((state) => state.generalData)
-  const [param, setParams] = useState(1)
-  const [load, setLoad] = useState(true)
-  const dispatch = useDispatch()
-  const category = categories.map((data) => (data.name))
-  const { url } = useParams()
-  const navigate = useNavigate()
+  const { allDatas, categories } = useSelector((state) => state.generalData);
+  const [param, setParams] = useState(1);
+  const [load, setLoad] = useState(true);
+  const dispatch = useDispatch();
+  const category = categories.map((data) => data.name);
+  const { url } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     switch (url) {
-      case undefined || 'NotFound': {
-        navigate('/tenetur')
-        setParams(1)
-        break
+      case undefined || "NotFound": {
+        navigate("/tenetur");
+        setParams(1);
+        break;
       }
       case "tenetur": {
-        setParams(1)
-        break
+        setParams(1);
+        break;
       }
       case "laudantium": {
-        setParams(2)
-        break
+        setParams(2);
+        break;
       }
       case "placeat": {
-        setParams(3)
-        break
+        setParams(3);
+        break;
       }
       case "corrupti": {
-        setParams(4)
-        break
+        setParams(4);
+        break;
       }
       default: {
-        navigate('NotFound')
+        navigate("NotFound");
       }
     }
-  }, [url, navigate])
-
+  }, [url, navigate]);
 
   useEffect(() => {
-    axios.get(`/category/${param}/posts`)
+    axios
+      .get(`/category/${param}/posts`)
       .then((response) => dispatch(allData(response.data)))
-      .catch(err => {
-        throw Error(err)
-      })
-
-  }, [dispatch, param])
-
+      .catch((err) => {
+        throw Error(err);
+      });
+  }, [dispatch, param]);
 
   useEffect(() => {
-    allDatas.length > 0 && setLoad(false)
-  }, [allDatas])
-
+    allDatas.length > 0 && setLoad(false);
+  }, [allDatas]);
 
   return (
     <>
       <Header />
-      <section className="data-section" >
+      <section className="data-section">
         <div className="container">
           <div className="data">
             <Aside />
             <div className="data__posts">
-              <h3 className="posts__title">
-                Recent Posts
-              </h3>
+              <h3 className="posts__title">Recent Posts</h3>
               {load && <Loading />}
               {allDatas.length > 0 &&
-                allDatas.map((data, index) => <div className="posts__wrapper" key={index}>
-                  <div className="date__wrapper">
-                    <time>{`${dayjs(data.createdAt).format('MMMM DD.YYYY')}`}</time>
-                    <span>{category[param - 1]}</span>
+                allDatas.map((data, index) => (
+                  <div className="posts__wrapper" key={index}>
+                    <div className="date__wrapper">
+                      <time>{`${dayjs(data.createdAt).format(
+                        "MMMM DD.YYYY"
+                      )}`}</time>
+                      <span>{category[param - 1]}</span>
+                    </div>
+                    <div className="post__title">
+                      <Link to={`${data.categoryId}/${index}`}>
+                        <h4>{data.title}</h4>
+                        <p>{data.description}</p>
+                      </Link>
+                    </div>
+                    <span className="post__time">
+                      {data.readTime} minutes read
+                    </span>
                   </div>
-                  <div className="post__title">
-                    <Link to={`${data.categoryId}/${index}`}>
-                      <h4>
-                        {data.title}
-                      </h4>
-                      <p>
-                        {data.description}
-                      </p>
-                    </Link>
-                  </div>
-                  <span className="post__time">
-                    {data.readTime} minutes read
-                  </span>
-                </div>)}
+                ))}
             </div>
-            <div className="posts__wrapper">
-            </div>
+            <div className="posts__wrapper"></div>
           </div>
           <div className="content__pagenation">
             <div>
@@ -113,7 +107,7 @@ function MainPage() {
       </section>
       <Footer />
     </>
-  )
+  );
 }
 
-export default MainPage
+export default MainPage;
